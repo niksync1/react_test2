121215397b4d95d34d0dbd54eee7bfa397763719
niksync1 <40718182+niksync1@users.noreply.github.com>
2023-05-11 15:06:33 +0000

    successfully modified with useContext

--- src/Layout.js
+++ src/Layout.js
@@ -2,8 +2,12 @@ import Header from './Header';
 import Nav from './Nav';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
+import { useContext } from 'react';
+import DataContext from './context/DataContext';
 
-const Layout = ({ search, setSearch, width }) => {
+const Layout = ({  width }) => {
+    const { search, setSearch,} = useContext(DataContext);
+
     return (
         <div className="App">
             <Header title="React JS Blog" width={width}/>
